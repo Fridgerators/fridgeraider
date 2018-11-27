a25e05dbcbed970543af0442f21154602a3db491
Lara Potjewyd <larapotjewyd@gmail.com>
2018-11-27 15:03:47 -0700

    set up the skeleton

---
+++ src/ducklings/reducer.js
@@ -0,0 +1,21 @@
+const initialState={
+   user:{}
+}
+
+const UPDATE_USER_DATA='UPDATE_USER_DATA';
+
+export function updateUserData(user) {
+   return {
+       type: UPDATE_USER_DATA,
+       payload: user
+   }
+}
+
+export default function reducer(state=initialState, action){
+   switch(action.type){
+       case UPDATE_USER_DATA:
+       return Object.assign({}, state, {user:action.payload})
+       default:
+       return state;
+   }
+}
