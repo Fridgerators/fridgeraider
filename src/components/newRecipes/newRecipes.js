50605dbf843742f48c8bda381bd58bad733345a7
unknown <jdavid.stevens@gmail.com>
2018-11-29 08:56:34 -0700

    Start-of-day

--- src/components/newRecipes/newRecipes.js
+++ src/components/newRecipes/newRecipes.js
@@ -5,30 +5,25 @@ class NewRecipes extends Component {
     constructor() {
         super()
         this.state = {
-            // searchIngredients: [],
             recipes: []
         }
     }
 
     async componentDidMount() {
-        let {searchIngredients} = this.props.match.params
-        console.log('params', searchIngredients)
-        // await axios.post(`/api/recipes/getResults`, searchIngredients)
+        let { searchIngredients } = this.props.match.params
         const { REACT_APP_AppID, REACT_APP_AppKey } = process.env;
-        // const dbInstance = req.app.get('db')
         axios.get(`https://api.edamam.com/search?q=${searchIngredients}&app_id=${REACT_APP_AppID}&app_key=${REACT_APP_AppKey}`)
-
-        .then(res=>
-            this.setState({recipes: res.data.hits}))
+            .then(res =>
+                this.setState({ recipes: res.data.hits }))
     }
 
 
     render() {
         console.log('recipes', this.state.recipes)
-        let recipeRes = this.state.recipes.map((element,index)=>{
-            return(
+        let recipeRes = this.state.recipes.map((element, index) => {
+            return (
                 <div key={index}>
-                    <img src={element.recipe.image} alt=''/>
+                    <img src={element.recipe.image} alt='' />
                     <h4>{element.recipe.label}</h4>
                 </div>
             )
