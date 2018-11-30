626262a6e3c6db8bf188f4e7468e221d88823182
Lara Potjewyd <larapotjewyd@gmail.com>
2018-11-29 17:14:48 -0700

    working on responsiveness

--- src/components/newIngredients/newIngredients.js
+++ src/components/newIngredients/newIngredients.js
@@ -62,24 +62,27 @@ class NewIngredients extends Component {
         }
         console.log(searchIngredients, typeof searchIngredients)
         console.log('searchIngredients',searchIngredients)
-        
+
         return (
             <div className="ni-bg">
                 <Nav />
                 <img className="header-curve" src={header} alt="background"/>
 
                 <h3 className="ni-title">enter the ingredients you have in your fridge and cupboards so we can find the perfect recipe for you!</h3>
+
+                <section className="ni-inputs-box" >
                     {newInput}
+                </section>
                 
                 <section className="ni-buttons">
                 <img src={remove} className='ni-remove-input' onClick={() => this.handleRemove(finalIndex)} />
                 <img src={add} className='ni-add-input' onClick={(e) => this.addIngredient(e)}/>
-                <img src={clear} className='ni-clear-input' alt="clear inputs"/>
+                <img src={clear} className='ni-clear-input' alt="clear inputs" onClick={this.handleClear} />
                 {searchIngredients ?
             
-            <Link to={`/results/${searchIngredients}`}><img src={search} className='ni-img'/></Link> :
+            <Link to={`/results/${searchIngredients}`}><img src={search} className='ni-search'/></Link> :
 
-            <img src={search} onClick={this.handleAlert} className='ni-img'/>
+            <img src={search} onClick={this.handleAlert} className='ni-search'/>
                
             }
             
