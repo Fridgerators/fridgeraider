6e213db23db9792698e4d505c302ab7afbfe181a
Lara Potjewyd <larapotjewyd@gmail.com>
2019-01-17 14:16:14 -0700

    some style changes to input boxes

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import logo from './logo-with-text.svg';
 
 class Landing extends Component{
+
     render(){
         return(
                 <div className='landing-page'>
@@ -10,7 +11,7 @@ class Landing extends Component{
                         <img className='landing-title' src={logo} alt=""/>
     
                         <h2 className='landing-sub'>find recipes based on what you have in your kitchen</h2>
-                        <Link to='/input'><button className="landing-button">enter</button></Link>
+                        <Link to='/input'><button className='landing-button'>enter</button></Link>
                     </div>
                 </div>
            
