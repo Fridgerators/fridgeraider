7d9dd1069a83bb8e30befc10dc8a7b9f4fc54059
Mitcheew <wowmormon@gmail.com>
2019-12-02 13:32:55 -0700

    minor adjustment to setupProxy

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api/**', {target: `${process.env.REACT_APP}`}))
-    app.use(proxy('/auth/**', {target: `${process.env.REACT_APP}`}))
+    app.use(proxy('/api/**', {target: `${process.env.REACT_APP}${process.env.SERVER_PORT}`}))
+    app.use(proxy('/auth/**', {target: `${process.env.REACT_APP}${process.env.SERVER_PORT}`}))
 }
